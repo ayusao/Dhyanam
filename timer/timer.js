60ee656477f67815a88cc2511f1a91b6a8b8deb1
ayusao <077bct006.aayusha@pcampus.edu.np>
2023-01-27 20:16:36 +0545

    pause/resume buttons added

--- timer/timer.js
+++ timer/timer.js
@@ -1,14 +1,12 @@
 
 let inputMinutes = document.getElementById("inputMinutes");
 let totalSeconds = 0;
-let countdownEl = document.getElementById('countdown');
+let isPaused = false;
+
+const countdownEl = document.getElementById('countdown');
+const stopBtn = document.getElementById('stopBtn');
+const resetBtn = document.getElementById('resetBtn');
 let intervalId;
-
-function startTimer() {
-    totalSeconds = inputMinutes.value * 60;
-    intervalId = setInterval(updateCountdown, 1000);
-}
-
 function updateCountdown() {
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds % 60;
@@ -20,4 +18,28 @@ function updateCountdown() {
         clearInterval(intervalId);
         countdownEl.innerHTML = "Time's Up!";
     }
+}
+function startTimer() {
+    totalSeconds = inputMinutes.value * 60;
+    intervalId = setInterval(updateCountdown, 1000);
+}
+function pauseResumeTimer() {
+    if (!isPaused) {
+        clearInterval(intervalId);
+        isPaused = true;
+        document.getElementById("pauseResume").innerHTML = "Resume";
+    } else {
+        intervalId = setInterval(updateCountdown, 1000);
+        isPaused = false;
+        document.getElementById("pauseResume").innerHTML = "Pause";
+    }
+}
+
+function stopTimer(){
+    clearInterval(intervalId);
+}
+function resetTimer(){
+    clearInterval(intervalId);
+    totalSeconds = inputMinutes.value * 60;
+    countdownEl.innerHTML = `${inputMinutes.value}:00`;
 }
